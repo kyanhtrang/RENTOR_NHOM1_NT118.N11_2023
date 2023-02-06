4ce9a19531fcdbcd56797d8437b72a25eaadcdc1
Trang Kỳ Anh <kyanhtrang0406@gmail.com>
2023-02-06 11:44:58 +0700

    nouse

---
+++ CarRenting-NT118/app/src/main/java/com/example/carrenting/ActivityPages/OwnerMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.carrenting.ActivityPages;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carrenting.R;
+
+public class OwnerMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.owner_activity_main);
+    }
+}
--- CarRenting-NT118/app/src/main/java/com/example/carrenting/ActivityPages/StartAppActivity.java
+++ CarRenting-NT118/app/src/main/java/com/example/carrenting/ActivityPages/StartAppActivity.java
@@ -102,7 +102,7 @@ public class StartAppActivity extends AppCompatActivity {
                     }
                 }
             });
-            Intent intent = new Intent(this, AddVehicleActivity.class);
+            Intent intent = new Intent(this, CustomerMainActivity.class);
             startActivity(intent);
         }
 
