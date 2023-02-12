e9266109bc9dcb106b62b903f8d1df87a926cc71
Trang Kỳ Anh <kyanhtrang0406@gmail.com>
2023-02-12 15:10:35 +0700

    up

---
+++ CarRenting-NT118/app/src/main/java/com/example/carrenting/Service/UserAuthentication/ProfileManagement.java
@@ -0,0 +1,16 @@
+package com.example.carrenting.Service.UserAuthentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.carrenting.R;
+
+public class ProfileManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_management);
+    }
+}
