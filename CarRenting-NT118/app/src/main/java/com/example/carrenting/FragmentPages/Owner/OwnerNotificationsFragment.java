a738ef4f908b4f1be59aee0a12c05a2f98007b42
Trang Kỳ Anh <kyanhtrang0406@gmail.com>
2023-02-18 09:15:48 +0700

    update

--- CarRenting-NT118/app/src/main/java/com/example/carrenting/FragmentPages/Owner/OwnerNotificationsFragment.java
+++ CarRenting-NT118/app/src/main/java/com/example/carrenting/FragmentPages/Owner/OwnerNotificationsFragment.java
@@ -16,6 +16,6 @@ public class OwnerNotificationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.customer_fragment_notification, container, false);
+        return inflater.inflate(R.layout.owner_fragment_notification, container, false);
     }
 }
