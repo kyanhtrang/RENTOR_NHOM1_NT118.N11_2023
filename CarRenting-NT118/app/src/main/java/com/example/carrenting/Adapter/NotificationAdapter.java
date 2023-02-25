b6ae3efde9f63fbdd16abce2b4c41cdca25ef702
Quý <80635501+quy-k15@users.noreply.github.com>
2023-02-25 14:43:34 +0700

    noti

--- CarRenting-NT118/app/src/main/java/com/example/carrenting/Adapter/NotificationAdapter.java
+++ CarRenting-NT118/app/src/main/java/com/example/carrenting/Adapter/NotificationAdapter.java
@@ -1,6 +1,7 @@
 package com.example.carrenting.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,51 +12,75 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.carrenting.FragmentPages.Customer.CustomerNotificationFragment;
+import com.example.carrenting.Model.Activity;
 import com.example.carrenting.R;
+import com.example.carrenting.Service.Activity.CustomerActivityDetail;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder> {
     private CustomerNotificationFragment customerNotificationFragment;
-    private ArrayList<String> title, content;
+//    private ArrayList<String> title, content;
+    Activity noti;
+    ArrayList<Activity> mNoti;
+    FirebaseFirestore dtb;
 
-    public NotificationAdapter(ArrayList<String> a, ArrayList<String> b) {
-        this.title = a;
-        this.content = b;
-    }
-    public NotificationAdapter(CustomerNotificationFragment mContext, ArrayList<String> a, ArrayList<String> b){
+//    public NotificationAdapter(ArrayList<String> a, ArrayList<String> b) {
+//        this.title = a;
+//        this.content = b;
+//    }
+    public NotificationAdapter(CustomerNotificationFragment mContext, ArrayList<Activity> mNoti){
         this.customerNotificationFragment = mContext;
-        this.title = a;
-        this.content =  b;
+        this.mNoti=mNoti;
     }
 
     @NonNull
     @Override
     public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_notification, parent, false);
-        return new NotificationViewHolder(view);
+        View v = LayoutInflater.from(customerNotificationFragment.getActivity()).inflate(R.layout.item_notification, parent, false);
+        return new NotificationViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
-        holder.vtitle.setText(title.get(position));
-        holder.vcontent.setText(content.get(position));
+        noti = mNoti.get(position);
+
+        if(noti.getStatus().equals( "Xac nhan"))
+        {
+            holder.status.setText("Nhà cung cấp đã xác nhận");
+        }
+        else
+        {
+//            if(noti.getStatus().equals( "Khong xac nhan"))
+//            {
+            holder.status.setText("Nhà cung cấp không xác nhận");
+//            }
+
+        }
+
+
+
     }
 
     @Override
     public int getItemCount() {
-        if(title != null)
-            return title.size();
-        return 0;
+        return mNoti.size();
     }
     public  class  NotificationViewHolder extends RecyclerView.ViewHolder{
-        private TextView vtitle;
-        private TextView vcontent;
+        TextView status;
+
         public NotificationViewHolder(@NonNull View itemView) {
             super(itemView);
-            vtitle = itemView.findViewById(R.id.item_notification_tv_title);
-            vcontent = itemView.findViewById(R.id.item_notification_tv_content);
+            status=itemView.findViewById(R.id.tv_noti_text);
+
         }
     }
+
+
 }
